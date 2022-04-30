bb75892240b65e71574670ea46c5e563b4f5b39d
BSEF19A520 <bsef19a520@pucit.edu.pk>
2022-05-01 02:27:18 +0500

    Updated

---
+++ ChangeTextOnButtonClick/app/src/main/java/com/example/tap/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text1;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text1 = (TextView) findViewById(R.id.text);
+    }
+
+    public void changeText(View view) {
+
+        text1.setText("Assalam Alikum ! ! !");
+    }
+}
